61d5c65dfc9e3140001d53716713e4da2caf4429
binbin.zhu <binbin.zhu@ushow.media>
2018-12-04 19:46:29 +0800

    Initial Commit

---
+++ IOS_bin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS_bin
+//
+//  Created by binbin on 2018/12/4.
+//  Copyright © 2018年 binbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS_bin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS_bin
+//
+//  Created by binbin on 2018/12/4.
+//  Copyright © 2018年 binbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
